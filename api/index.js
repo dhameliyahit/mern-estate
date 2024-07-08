c867ff1ea1b65ff37717071aeed7b8e367c34261
Dhameliyahit <153006638+dhameliyahit@users.noreply.github.com>
2024-07-08 10:16:22 +0530

    connect to dataase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("database connected successfully")
+}).catch(()=>{
+    console.log("Error while connect database")
+})
 const app = express();
+
 
 
 app.listen(3000,()=>{
