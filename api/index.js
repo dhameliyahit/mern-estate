2014c60c05b426aa85add231627bbef6fcd4b664
Dhameliyahit <153006638+dhameliyahit@users.noreply.github.com>
2024-07-16 20:26:49 +0530

    create a middle were tand function' for error handling

--- api/index.js
+++ api/index.js
@@ -22,6 +22,17 @@ app.get("/",(req,res)=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500 ;
+    const message = err.message ||  'Internal Server Error'
+
+    return  res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
+});
+
 app.listen(3000,()=>{
     console.log("server is running to 3000");
 })
